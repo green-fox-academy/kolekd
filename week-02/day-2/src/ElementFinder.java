8d3084ff321bc85160f70af0291497584e7a70ac
Daniel <kolekd007@gmail.com>
2018-09-25 17:45:47 +0200

    Element Finder objective

---
+++ week-02/day-2/src/ElementFinder.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ElementFinder{
+    public static void main(String... args){
+
+        // Write a method that checks if the arrayList contains "7"
+        //          if it contains return "Hoorray" otherwise return "Noooooo"
+        // The output should be: "Noooooo"
+        // Do this again with a different solution using different list methods!
+
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(containsSeven(arrayList));
+
+
+
+    }
+}
