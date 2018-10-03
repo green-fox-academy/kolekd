c0d5c69c3b3dc020b33952c51ea808383434690f
Daniel <kolekd007@gmail.com>
2018-10-03 13:49:55 +0200

    BlogPost - completed

--- week-03/day-3/src/Blog.java
+++ week-03/day-3/src/Blog.java
@@ -1,7 +1,22 @@
 public class Blog {
     public static void main (String[] args){
 
-        BlogPost Lorem = new BlogPost("unknown","unknown","unknown","unknown")
+        BlogPost Lorem = new BlogPost("John Doe","Lorem Ipsum","2000.05.04.",
+                                "Lorem ipsum dolor sit amet.");
 
+        BlogPost Wait = new BlogPost("Tim Urban","Wait but why","2010.10.10.",
+                                "A popular long-form, stick-figure-illustrated blog about almost everything.");
+
+        BlogPost Engineer = new BlogPost("William Turton", "One Engineer Is Trying to " +
+                                        "Get IBM to Reckon With Trump", "2017.03.28.",
+                                        "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t " +
+                                                "want to be the center of attention. When I asked to " +
+                                                "take his picture outside one of IBM’s New York City " +
+                                                "offices, he told me that he wasn’t really into the whole" +
+                                                " organizer profile thing.");
+
+        Lorem.printBlog();
+        Wait.printBlog();
+        Engineer.printBlog();
     }
 }
--- week-03/day-3/src/BlogPost.java
+++ week-03/day-3/src/BlogPost.java
@@ -9,11 +9,15 @@ public class BlogPost {
         this("unknown","unknown","unknown","unknown");
     }
 
-    public BlogPost(String authorName, String title, String text, String publicationDate){
+    public BlogPost(String authorName, String title, String publicationDate, String text){
         this.authorName = authorName;
         this.title = title;
         this.text = text;
         this.publicationDate = publicationDate;
+    }
+
+    public void printBlog(){
+        System.out.println(title + " titled by " + authorName + " posted at \"" + publicationDate + "\"" + "\n" + text);
     }
 
 }
