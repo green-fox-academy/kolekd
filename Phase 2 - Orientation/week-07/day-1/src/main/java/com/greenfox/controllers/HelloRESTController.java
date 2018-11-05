0d4582af97023ff4d5a351676fe5f6cdff46b5c2
Daniel <kolekd007@gmail.com>
2018-11-05 17:11:33 +0100

    All exercises completed

--- Phase 2 - Orientation/week-07/day-1/src/main/java/com/greenfox/controllers/HelloRESTController.java
+++ Phase 2 - Orientation/week-07/day-1/src/main/java/com/greenfox/controllers/HelloRESTController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloRESTController {
 
     AtomicLong numberInit = new AtomicLong(0);
+    int zero = 0;
 
     @RequestMapping
     public Greeting greeting(@RequestParam("name") String name){
@@ -23,6 +25,40 @@ public class HelloRESTController {
 
     public long getCount(){
         return numberInit.incrementAndGet();
+    }
+
+    public int get1to50 () {
+        if (zero > 48){
+            zero = -1;
+        }
+        zero++;
+        return zero;
+    }
+
+    public String getRandLineOf6(){
+        Integer rand6 = 0;
+        rand6 = (int)(Math.random() * ((999999 - 100000) + 1)) + 100000;
+        return "#" + rand6.toString();
+    }
+
+    public String getRand25to50(){
+        Integer rand1050 = 0;
+        rand1050 = (int)(Math.random() * ((50 - 25) + 1)) + 25;
+        return rand1050.toString();
+    }
+
+    public String getFont(){
+        ArrayList<String> fonts = new ArrayList<>();
+        fonts.add("Times New Roman");
+        fonts.add("Verdana");
+        fonts.add("Comic sans MS");
+        fonts.add("WildWest");
+        fonts.add("Bedrock");
+        return fonts.get(getRand0toX(4));
+    }
+
+    public int getRand0toX(int x){
+        return (int)(Math.random() * ((x - 0) + 1)) + 0;
     }
 
 }
--- Phase 2 - Orientation/week-07/day-1/src/main/java/com/greenfox/controllers/HelloWebController.java
+++ Phase 2 - Orientation/week-07/day-1/src/main/java/com/greenfox/controllers/HelloWebController.java
@@ -9,8 +9,17 @@ public class HelloWebController {
 
     HelloRESTController rest = new HelloRESTController();
 
+    String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+
     @RequestMapping("/web/greeting")
     public String greeting(Model model) {
+        model.addAttribute("font", rest.getFont());
+        model.addAttribute("size", rest.getRand25to50());
+        model.addAttribute("colour", rest.getRandLineOf6());
+        model.addAttribute("hello", hellos[rest.get1to50()]);
         model.addAttribute("name", rest.getName("Daniel"));
         model.addAttribute("number", rest.getCount());
         return "greeting";
